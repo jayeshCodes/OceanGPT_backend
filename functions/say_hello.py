88165229bf5dd8a23ad9a624948d4b4e62faf97e
Jayesh Gajbhar <jaygajbhar@gmail.com>
2024-10-24 22:05:35 -0400

    minor modification : say_hello.py

--- functions/say_hello.py
+++ functions/say_hello.py
@@ -1,3 +1,6 @@
 def say_hello():
-    """test function to see if function calling works as expected"""
+    """
+    test function to see if function calling works as expected
+    say hi to the llm to test
+    """
     print("Hello World!")
